a8b6b7e64bb8db4aa3120bedb6eec37dd6052151
ManojPathak1 <manoj.pathak1215@gmail.com>
2020-05-22 23:38:12 +0530

    Typescript integration

--- src/components/common/TagsInput/TagsInput.tsx
+++ src/components/common/TagsInput/TagsInput.tsx
@@ -7,27 +7,27 @@ import s from "./TagsInput.scss";
  * @param {Array<String>} value Holds the array of strings of tags
  * @param {Function(Array<String>)} onChange Function called with updated tags  
  */
-function TagsInput({ label, value = [], onChange }) {
+function TagsInput({ label, value = [], onChange }: { label: string, value: any, onChange: any }) {
   const [inputText, setInputText] = useState("");
-  const handleKeyDown = (e) => {
+  const handleKeyDown = (e: any) => {
     if (e.key === 'Enter') {
-      const updatedTags = [...value, ...e.target.value.split(",").map(el => el.trim())];
+      const updatedTags = [...value, ...e.target.value.split(",").map((el: any) => el.trim())];
       setInputText("");
       onChange(updatedTags);
     }
   };
-  const deleteTag = index => {
+  const deleteTag = (index: number) => {
     const newTags = [...value];
     newTags.splice(index, 1);
     onChange(newTags);
   };
-  console.log(s);
+  console.log(label);
   return (
     <div className={s.tagsInputContainer}>
       <label>{label}</label>
       <input className="inputText" type="text" value={inputText} onChange={(event) => setInputText(event.target.value)} onKeyDown={handleKeyDown} />
       <div>
-        {value.map((tag, i) => <span key={i}>{tag} <img alt="" onClick={() => deleteTag(i)} height="9" src="./images/close.png" /></span>)}
+        {value.map((tag: any, i: number) => <span key={i}>{tag} <img alt="" onClick={() => deleteTag(i)} height="9" src="./images/close.png" /></span>)}
       </div>
     </div>
   );
